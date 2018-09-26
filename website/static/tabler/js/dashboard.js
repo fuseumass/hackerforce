b4c89afd4e29d1c0ca5a25ee201e7305d2698b3f
Nicholas Williams <nawilliams@umass.edu>
2018-09-26 07:27:25 -0400

    fix(tabler): use correct route to static assets in dashboard.js

--- website/static/tabler/js/dashboard.js
+++ website/static/tabler/js/dashboard.js
@@ -9,16 +9,16 @@ shim: {
 'core': ['bootstrap', 'jquery'],
 },
 paths: {
-'core': 'assets/js/core',
-'jquery': 'assets/js/vendors/jquery-3.2.1.min',
-'bootstrap': 'assets/js/vendors/bootstrap.bundle.min',
-'sparkline': 'assets/js/vendors/jquery.sparkline.min',
-'selectize': 'assets/js/vendors/selectize.min',
-'tablesorter': 'assets/js/vendors/jquery.tablesorter.min',
-'vector-map': 'assets/js/vendors/jquery-jvectormap-2.0.3.min',
-'vector-map-de': 'assets/js/vendors/jquery-jvectormap-de-merc',
-'vector-map-world': 'assets/js/vendors/jquery-jvectormap-world-mill',
-'circle-progress': 'assets/js/vendors/circle-progress.min',
+'core': 'static/tabler/js/core',
+'jquery': 'static/tabler/js/vendors/jquery-3.2.1.min',
+'bootstrap': 'static/tabler/js/vendors/bootstrap.bundle.min',
+'sparkline': 'static/tabler/js/vendors/jquery.sparkline.min',
+'selectize': 'static/tabler/js/vendors/selectize.min',
+'tablesorter': 'static/tabler/js/vendors/jquery.tablesorter.min',
+'vector-map': 'static/tabler/js/vendors/jquery-jvectormap-2.0.3.min',
+'vector-map-de': 'static/tabler/js/vendors/jquery-jvectormap-de-merc',
+'vector-map-world': 'static/tabler/js/vendors/jquery-jvectormap-world-mill',
+'circle-progress': 'static/tabler/js/vendors/circle-progress.min',
 }
 });
 window.tabler = {
