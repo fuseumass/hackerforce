7ccbf3b320f936f9055e2d70478477c39c1ffee0
Kevin Fredericks <kj.fredericks2812@gmail.com>
2018-09-28 16:05:32 -0400

    Added drafts, outbox, and sent pages

--- core/urls.py
+++ core/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('companies', views.companies, name='companies'),
     path('contacts', views.contacts, name='contacts'),
     path('email', views.email, name='email'),
+    path('drafts', views.drafts, name='drafts'),
+    path('outbox', views.outbox, name='outbox'),
+    path('sent', views.sent, name='sent'),
     path('settings', views.settings, name='settings'),
 ]
