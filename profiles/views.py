09010bd08f578a372b5469fd2279360150fa0016
ikhatri <ishan@khatri.io>
2018-11-03 21:19:53 -0400

    Added views for signing in and registration

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 
+def register(request):
+    return render(request, 'register.html.j2')
+
+def login(request):
+    return render(request, 'login.html.j2')
 
 def settings(request):
     return render(request, 'settings.html.j2')
--- website/urls.py
+++ website/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 from django.contrib import admin
 admin.autodiscover()
 
+from profiles.views import register, login
+
 urlpatterns = [
     path('', include('dashboard.urls')),
     path('admin/', admin.site.urls),
@@ -26,4 +28,6 @@ urlpatterns = [
     path('dashboard/', include('dashboard.urls')),
     path('email/', include('emails.urls')),
     path('settings/', include('profiles.urls')),
+    path('register/', register, name='register'),
+    path('login/', login, name='login')
 ]
