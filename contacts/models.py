36f5dd6bfb5f97004c121b1b8b80280fb4c1184d
Nicholas Williams <nawilliams@umass.edu>
2018-11-06 09:37:57 -0500

    feat(contacts): makemigration and fix phonenumberfield import

--- contacts/models.py
+++ contacts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 class Person(models.Model):
     first_name = models.CharField(max_length=50)
@@ -7,6 +8,9 @@ class Person(models.Model):
     phone_number = PhoneNumberField(blank=True)
     email = models.EmailField()
     is_warm_contact = models.BooleanField()
-    position = models.CharField(max_lengt=100)
+    position = models.CharField(max_length=100)
     company = models.CharField(max_length=100) # replace with assosciation
     # sponsorships field
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
