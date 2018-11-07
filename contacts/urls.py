1a3103054f08d42a78433c1504e90994dfdf91cd
Nicholas Williams <nawilliams@umass.edu>
2018-11-07 10:47:16 -0500

    models(contacts): views, urls, forms for edit and new pages

--- contacts/urls.py
+++ contacts/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
-app_name = 'contacts'
+app_name = "contacts"
 urlpatterns = [
-    path('', views.contacts, name='index'),
+    path("", views.contacts, name="index"),
+    path("new", views.contact_new, name="new"),
+    path("<int:pk>/edit", views.contact_edit, name="edit"),
 ]
