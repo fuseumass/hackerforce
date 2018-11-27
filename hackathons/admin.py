bd9cb28e11c9e0c8ff1d265852e44bf885fdda4a
Nicholas Williams <nawilliams@umass.edu>
2018-11-27 13:30:37 -0500

    models(hackathons): create hackathon models

--- hackathons/admin.py
+++ hackathons/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Tier, Perk, Hackathon, Sponsorship
 
 # Register your models here.
+
+admin.site.register(Tier)
+admin.site.register(Perk)
+admin.site.register(Hackathon)
+admin.site.register(Sponsorship)
--- hackathons/models.py
+++ hackathons/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+from companies.models import Company
+
+
+class Tier(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Perk(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+
+class Hackathon(models.Model):
+    name = models.CharField(max_length=255)
+    date = models.DateField(blank=True)
+    fundraising_goal = models.IntegerField(blank=True)
+    tiers = models.ManyToManyField(Tier, null=True, blank=True)
+    perks = models.ManyToManyField(Perk, null=True, blank=True)
+
+
+class Sponsorship(models.Model):
+    STATUSES = (
+        ("uncontacted", "Uncontacted"),
+        ("contacted", "Contacted"),
+        ("donated", "Donated"),
+    )
+
+    hackathon = models.ForeignKey(
+        Hackathon, on_delete=models.CASCADE, related_name="sponsorships"
+    )
+    company = models.ForeignKey(
+        Company, on_delete=models.CASCADE, related_name="sponsorships"
+    )
+
+    contribution = models.IntegerField()
+    status = models.CharField(max_length=10, choices=STATUSES)
+
+    tier = models.ForeignKey(
+        Tier,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="sponsorships",
+    )
+    perks = models.ManyToManyField(Perk, null=True, blank=True)
+
