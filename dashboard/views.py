a97e4d60e7b4c5cf5edbd27446174fafc8e63251
Ishan Khatri <ikhatrix@gmail.com>
2018-11-30 00:45:29 -0500

    Dashboard page updates the funding progress bar dynamically based on the latest hackathon & the associated sponsorships

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from hackathons.models import Sponsorship, Hackathon
+
 # @login_required
 def dashboard(request):
-    return render(request, 'dashboard.html.j2')
+  current_hackathon = Hackathon.objects.latest('date')
+  sponsorships = Sponsorship.objects.filter(hackathon=current_hackathon)
+  money_raised = 0
+  for sponsorship in sponsorships:
+    money_raised = money_raised + sponsorship.contribution
+  return render(request, 'dashboard.html.j2', {"current_hackathon":current_hackathon, "money_raised":money_raised})
