a316264da294b8986f6370c535042492270ac3aa
Nicholas Williams <nawilliams@umass.edu>
2018-12-05 19:04:52 -0500

    forms(companies): fix picker for industries

--- companies/forms.py
+++ companies/forms.py
@@ -3,67 +3,53 @@ from .models import Company, Industry
 from contacts.models import Contact
 import datetime
 
+
 class CompanyForm(forms.ModelForm):
     name = forms.CharField(
         max_length=50,
         required=True,
         widget=forms.TextInput(
-            attrs={
-                "class": "form-control col-md-6 col-lg-4",
-                "placeholder": "Name",
-            }
+            attrs={"class": "form-control col-md-6 col-lg-4", "placeholder": "Name"}
         ),
     )
+
     donated = forms.IntegerField(
         required=True,
         widget=forms.TextInput(
             attrs={"class": "form-control col-md-6 col-lg-4", "placeholder": "Donated"}
         ),
     )
-    #updated = datetime.datetime.now()
-    industries = ("industries")
+
+    industries = forms.ModelMultipleChoiceField(
+        required=True,
+        queryset=Industry.objects.all(),
+        widget=forms.SelectMultiple(attrs={"class": "custom-select col-md-6 col-lg-4"}),
+    )
+
     location = forms.CharField(
         max_length=140,
         required=True,
         widget=forms.TextInput(
-            attrs={
-                "class": "form-control col-md-6 col-lg-4",
-                "placeholder": "Location",
-            }
+            attrs={"class": "form-control col-md-6 col-lg-4", "placeholder": "Location"}
         ),
     )
+
     status = forms.ChoiceField(
         required=True,
-        choices=(("U", "Uncontacted"), ("C", "Contacted"), ("D", "Donated")),
+        choices=(Company.STATUSES),
         widget=forms.Select(
             attrs={"class": "custom-select col-md-6 col-lg-4", "placeholder": "Status"}
         ),
     )
+
     size = forms.ChoiceField(
-        #required=True,
-        choices=(("L", "Large"), ("M", "Medium"), ("S", "Small")),
+        required=True,
+        choices=(Company.SIZES),
         widget=forms.Select(
             attrs={"class": "custom-select col-md-6 col-lg-4", "placeholder": "Size"}
         ),
     )
-    updated = forms.DateField()
 
-    fields = ("industries")
-
-    def __init__(self, *args, **kwargs):
-
-        super(CompanyForm, self).__init__(*args, **kwargs)
-
-        self.fields["industries"].widget = forms.CheckboxSelectMultiple()
-        self.fields["industries"].queryset = Industry.objects.all()
     class Meta:
         model = Company
-        fields = (
-            "name",
-            "donated",
-            "industries",
-            "location",
-            "status",
-            "size",
-            "updated"
-        )
+        fields = ("name", "donated", "industries", "location", "status", "size")
--- companies/views.py
+++ companies/views.py
@@ -2,23 +2,29 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Company
 from .forms import CompanyForm
 
+
 def new(request):
-    return render(request, 'new.html.j2')
+    return render(request, "new.html.j2")
+
 
 def companies(request):
     companies = Company.objects.all()
-    return render(request, 'companies.html.j2', context = {'companies' : companies})
+    return render(request, "companies.html.j2", context={"companies": companies})
+
 
 def company_new(request):
     if request.method == "POST":
         form = CompanyForm(request.POST)
+        print(form)
         if form.is_valid():
             company = form.save(commit=False)
+            company.industries.set(form.cleaned_data["industries"])
             company.save()
             return redirect("companies:index")
     else:
         form = CompanyForm()
     return render(request, "company_new.html.j2", {"form": form})
+
 
 def company_edit(request, pk):
     company = get_object_or_404(Company, pk=pk)
@@ -26,6 +32,7 @@ def company_edit(request, pk):
         form = CompanyForm(request.POST, instance=company)
         if form.is_valid():
             company = form.save(commit=False)
+            company.industries.set(form.cleaned_data["industries"])
             company.save()
             return redirect("companies:index")
     else:
