7679d4a354cacb749cc84343b15b2f36c5e760bb
Nicholas Williams <nawilliams@umass.edu>
2018-12-11 23:29:07 -0500

    docs(final): add urls/routes section

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 
 from . import views
 
-app_name = 'dashboard'
+app_name = "dashboard"
 urlpatterns = [
-    path('', views.dashboard, name='index'),
-    path('404', views.page404, name='404'),
+    path("", views.dashboard, name="index"),
+    path("404", views.page404, name="404"),
 ]
