fc0fccf499720820da0004b26145d2f51cb3fb28
James Woglom <j@wogloms.net>
2019-06-27 01:05:26 -0700

    jinja2 to django migration tools

--- website/settings.py
+++ website/settings.py
@@ -103,6 +103,9 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
+            'builtins': [
+                'django.contrib.staticfiles.templatetags.staticfiles'
+            ],
             "debug": DEBUG,
         },
     },
