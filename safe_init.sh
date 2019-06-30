84f6b23822affa26c8ce622e00e3d7edbec3851b
James Woglom <j@wogloms.net>
2019-06-30 02:09:44 -0700

    run migrate

--- safe_init.sh
+++ safe_init.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 python manage.py shell -c'from profiles.models import User; User.objects.get(username="admin")';
 if [[ "$?" == "1" ]]; then
-  echo Running init script
+  echo Empty database. Running init script...
   chmod +x init.sh
   ./init.sh
 else
-  echo Admin user exists
+  echo Admin user exists. Running migrate...
+  python manage.py migrate
 fi
