2fd9e748fe5d795e11d2cded01a670280a4e7848
Ishan Khatri <ikhatrix@gmail.com>
2019-06-30 02:10:34 -0400

    Added a company detail view

--- companies/urls.py
+++ companies/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import companies, company_new, company_edit
+from .views import companies, company_new, company_edit, company_detail
 
 app_name = 'companies'
 urlpatterns = [
     path('', companies, name='index'),
     # path('new', views.new, name='new')
     path("new", company_new, name="new"),
-    path("<int:pk>/edit", company_edit, name="edit")
+    path("<int:pk>/edit", company_edit, name="edit"),
+    path("<int:pk>/view", company_detail, name="view")
 ]
