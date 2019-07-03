8d6f61f20398cf4c622ff109e3b17120cf2a8236
Ishan Khatri <ishan@khatri.io>
2019-07-03 03:58:56 -0400

    New & improved email model + setup for 3 compose forms

--- emails/urls.py
+++ emails/urls.py
@@ -4,10 +4,5 @@ from . import views
 
 app_name = 'emails'
 urlpatterns = [
-    path("", views.emails, name="index"),
-    path("drafts", views.drafts, name="drafts"),
-    path("outbox", views.outbox, name="outbox"),
-    path("sent", views.sent, name="sent"),
-    path("<int:pk>/edit", views.email_edit, name="edit"),
-     path("<int:pk>/sent_view", views.sent_view, name="sent_view"),
+    path("", views.emails, name="compose")
 ]
