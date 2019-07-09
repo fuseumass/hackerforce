145293b07c3edf92514947d9cdd90e8c556056cc
ikhatri <ishan@khatri.io>
2019-07-09 01:07:43 -0400

    more email views

--- emails/views.py
+++ emails/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 from hackathons.models import Hackathon
 from .models import Email
 from .forms import Compose1, Compose2, Compose3
 
 # Create your views here.
+@login_required
 def emails(request, h_pk):
   hackathon = get_object_or_404(Hackathon, pk=h_pk)
   return render(request, "email_compose.html", {"h" : hackathon})
 
+@login_required
 def email_detail(request, h_pk, pk):
   email = get_object_or_404(Email, pk=pk)
   hackathon = get_object_or_404(Hackathon, pk=h_pk)
   return render(request, "email_detail_view.html", {"email": email, "hackathon": hackathon})
 
+@login_required
 def drafts(request, h_pk):
   hackathon = get_object_or_404(Hackathon, pk=h_pk)
   emails = Email.objects.filter(hackathon=hackathon)
@@ -28,9 +32,11 @@ def drafts(request, h_pk):
   emails = paginator.get_page(page)
   return render(request, "email_drafts.html", {"emails" : emails})
 
+@login_required
 def sent(request, h_pk):
   return render(request, "email_sent.html")
 
+@login_required
 def compose1(request, h_pk):
   hackathon = get_object_or_404(Hackathon, pk=h_pk)
   if request.method == "POST":
@@ -40,12 +46,15 @@ def compose1(request, h_pk):
       email.hackathon = hackathon
       email.status = 'draft'
       email.save()
+      email.to_contacts.set(form.cleaned_data["to_contacts"])
+      email.save()
       messages.success(request, f"Created email with subject: {email.subject}")
       return redirect("emails:drafts", h_pk=h_pk)
   else:
     form = Compose1()
   return render(request, "email_compose1.html", {"form" : form})
 
+@login_required
 def compose2(request, h_pk):
   hackathon = get_object_or_404(Hackathon, pk=h_pk)
   if request.method == "POST":
@@ -55,12 +64,15 @@ def compose2(request, h_pk):
       email.hackathon = hackathon
       email.status = 'draft'
       email.save()
+      email.to_companies.set(form.cleaned_data["to_companies"])
+      email.save()
       messages.success(request, f"Created email with subject: {email.subject}")
       return redirect("emails:drafts", h_pk=h_pk)
   else:
     form = Compose2()
   return render(request, "email_compose2.html", {"form": form})
 
+@login_required
 def compose3(request, h_pk):
   hackathon = get_object_or_404(Hackathon, pk=h_pk)
   if request.method == "POST":
@@ -69,6 +81,8 @@ def compose3(request, h_pk):
       email = form.save(commit=False)
       email.hackathon = hackathon
       email.status = 'draft'
+      email.save()
+      email.to_industries.set(form.cleaned_data["to_industries"])
       email.save()
       messages.success(request, f"Created email with subject: {email.subject}")
       return redirect("emails:drafts", h_pk=h_pk)
