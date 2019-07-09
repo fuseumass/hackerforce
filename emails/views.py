1eeb51f2ed1b2837acd710ad617e22833aea3d8f
ikhatri <ishan@khatri.io>
2019-07-08 23:37:19 -0400

    more email compose stuff

--- emails/views.py
+++ emails/views.py
@@ -3,12 +3,17 @@ from django.contrib import messages
 from django.core.paginator import Paginator
 from hackathons.models import Hackathon
 from .models import Email
-from .forms import Compose1
+from .forms import Compose1, Compose2, Compose3
 
 # Create your views here.
 def emails(request, h_pk):
   hackathon = get_object_or_404(Hackathon, pk=h_pk)
   return render(request, "email_compose.html", {"h" : hackathon})
+
+def email_detail(request, h_pk, pk):
+  email = get_object_or_404(Email, pk=pk)
+  hackathon = get_object_or_404(Hackathon, pk=h_pk)
+  return render(request, "email_detail_view.html", {"email": email, "hackathon": hackathon})
 
 def drafts(request, h_pk):
   hackathon = get_object_or_404(Hackathon, pk=h_pk)
@@ -33,15 +38,40 @@ def compose1(request, h_pk):
     if form.is_valid():
       email = form.save(commit=False)
       email.hackathon = hackathon
+      email.status = 'draft'
       email.save()
       messages.success(request, f"Created email with subject: {email.subject}")
-      redirect("emails:drafts", h_pk)
+      return redirect("emails:drafts", h_pk=h_pk)
   else:
     form = Compose1()
   return render(request, "email_compose1.html", {"form" : form})
 
 def compose2(request, h_pk):
-  return render(request, "email_compose2.html")
+  hackathon = get_object_or_404(Hackathon, pk=h_pk)
+  if request.method == "POST":
+    form = Compose2(request.POST)
+    if form.is_valid():
+      email = form.save(commit=False)
+      email.hackathon = hackathon
+      email.status = 'draft'
+      email.save()
+      messages.success(request, f"Created email with subject: {email.subject}")
+      return redirect("emails:drafts", h_pk=h_pk)
+  else:
+    form = Compose2()
+  return render(request, "email_compose2.html", {"form": form})
 
 def compose3(request, h_pk):
-  return render(request, "email_compose3.html")
+  hackathon = get_object_or_404(Hackathon, pk=h_pk)
+  if request.method == "POST":
+    form = Compose3(request.POST)
+    if form.is_valid():
+      email = form.save(commit=False)
+      email.hackathon = hackathon
+      email.status = 'draft'
+      email.save()
+      messages.success(request, f"Created email with subject: {email.subject}")
+      return redirect("emails:drafts", h_pk=h_pk)
+  else:
+    form = Compose3()
+  return render(request, "email_compose3.html", {"form": form})
