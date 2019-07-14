dcb32ab0c8a632658c6db2f53547a16ccbec7961
James Woglom <j@wogloms.net>
2019-07-14 15:29:23 -0700

    login_required

--- hackathons/views/hackathons.py
+++ hackathons/views/hackathons.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from ..models import Hackathon
 from ..forms import HackathonForm
 
+@login_required
 def hackathons(request):
     hackathons = Hackathon.objects.all()
     return render(request, "hackathons.html", {"hackathons": hackathons})
 
+@login_required
 def hackathon_new(request):
     if request.method == "POST":
         form = HackathonForm(request.POST)
@@ -21,6 +24,7 @@ def hackathon_new(request):
         form = HackathonForm()
     return render(request, "hackathon_new.html", {"form": form})
 
+@login_required
 def hackathon_edit(request, h_pk):
     hackathon = get_object_or_404(Hackathon, pk=h_pk)
     if request.method == "POST":
